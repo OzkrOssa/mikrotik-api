3f6c6217897679d56e48d861d484ba1afdb51b12
OzkrOssa <oskrlex17@gmail.com>
2022-02-18 10:55:59 -0500

    ✔

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ def byExcel():
 @byExcel.command()
 @click.argument('profile')
 @click.option('-d','--dir', default='Documents', help='Directorio donde se encuentra el archivo excel')
-@click.option('-f','--file', default='Documents', help='Nombre del archivo excel')
+@click.option('-f','--file', default='habilitar', help='Nombre del archivo excel')
+
 def fromExcel(profile, dir, file):
     print ('setProfile', profile,dir, file)
 
@@ -32,7 +33,6 @@ def bySQL():
 @bySQL.command()
 @click.argument('profile')
 @click.option('-i','--id', help='Codigo del abonado', multiple=True)
-@click.option('-f','--file', default='Documents', help='Nombre del archivo excel')
 
 def fromSQL(profile, id, file):
     data = list(map(int, list(id)))
