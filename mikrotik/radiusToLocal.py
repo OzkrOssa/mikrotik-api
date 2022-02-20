eccb3e8ee76e088737fdae28399db564f780b832
Oscar Ossa <oskrlex17@gmail.com>
2022-02-20 16:05:59 -0500

    env variables dor this file

--- mikrotik/radiusToLocal.py
+++ mikrotik/radiusToLocal.py
@@ -1,30 +1,8 @@
 from Bts import *
-import os
-from dotenv import load_dotenv
+from modules.env_variables import MIKROTIK_IPS,API_KEY,API_USER
 
-
-IPS = [
-        os.getenv('BTS_200'),
-        os.getenv('BTS_220'),
-        os.getenv('BTS_230'),
-        os.getenv('BTS_MORON'),
-        os.getenv('BTS_CALERA'),
-        os.getenv('BTS_TABUYO'),
-        os.getenv('BTS_BLANDON'),
-        os.getenv('BTS_IBERIA'),
-        os.getenv('BTS_GUAMAL'),
-        os.getenv('BTS_AFRICA'),
-        os.getenv('BTS_SUPIA'),
-        os.getenv('BTS_IRRAFO'),
-        os.getenv('BTS_IRRAINA'),
-        os.getenv('BTS_SL'),
-        os.getenv('BTS_CRUCES'),
-        os.getenv('BTS_PV')      
-    ]
-
-load_dotenv()
-for allBts in IPS:
-    bts= Bts(allBts, 'api', '1017230619')
+for allBts in MIKROTIK_IPS:
+    bts= Bts(allBts, API_USER, API_USER)
     users = bts.getPppActiveUsers()
     users = list(users)
 
