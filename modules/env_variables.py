5120b3e523089512faa74f6e19b54a3cdf1fece5
OzkrOssa <oskrlex17@gmail.com>
2022-02-21 11:32:53 -0500

    coment some ips

--- modules/env_variables.py
+++ modules/env_variables.py
@@ -4,21 +4,21 @@ import os
 load_dotenv()
 
 MIKROTIK_IPS = [
-        os.getenv('BTS_200'),
-        os.getenv('BTS_220'),
-        os.getenv('BTS_230'),
+        #os.getenv('BTS_200'),
+        #os.getenv('BTS_220'),
+        #os.getenv('BTS_230'),
         os.getenv('BTS_MORON'),
-        os.getenv('BTS_CALERA'),
+        #os.getenv('BTS_CALERA'),
         os.getenv('BTS_TABUYO'),
-        os.getenv('BTS_BLANDON'),
-        os.getenv('BTS_IBERIA'),
-        os.getenv('BTS_GUAMAL'),
-        os.getenv('BTS_SUPIA'),
-        os.getenv('BTS_IRRAFO'),
-        os.getenv('BTS_IRRAINA'),
-        os.getenv('BTS_SL'),
-        os.getenv('BTS_CRUCES'),
-        os.getenv('BTS_PV')     
+        #os.getenv('BTS_BLANDON'),
+        #os.getenv('BTS_IBERIA'),
+        #os.getenv('BTS_GUAMAL'),
+        #os.getenv('BTS_SUPIA'),
+        #os.getenv('BTS_IRRAFO'),
+        #os.getenv('BTS_IRRAINA'),
+        #os.getenv('BTS_SL'),
+        #os.getenv('BTS_CRUCES'),
+        #os.getenv('BTS_PV')     
     ]
 API_USER = os.getenv('API_USER')
 API_KEY = os.getenv('API_KEY')
